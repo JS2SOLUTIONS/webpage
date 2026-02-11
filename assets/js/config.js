5642fc23ed0e187945af8d211377ca7439832bc2
JS2 TECH SOLUTIONS SAS <js2solutions@outlook.com>
2026-02-11 11:51:19 -0500

    Add files via upload

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 const SITE_CONFIG = {
     headerName: "JS2 SOLUTIONS",
     companyName: "JS2 TECH SOLUTIONS S.A.S",
-    email: "contacto@js2solutions.com.co",
+    email: "js2solutions@outlook.com",
     year: new Date().getFullYear(),
     logoSrc: "assets/images/logo.png",
     heroVideoSrc: "assets/video/hero2.mp4",
@@ -10,7 +10,6 @@ const SITE_CONFIG = {
     whatsapp: "+573002584013",
     whatsappLink: "https://wa.me/573002584013",
     linkedinLink: "https://www.linkedin.com/company/js2-solutions",
-    emailLink: "mailto:contacto@js2solutions.com.co"
+    emailLink: "mailto:js2solutions@outlook.com"
 
 };
-
